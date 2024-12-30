ee241032f8863e6374e62ca0330e354654b18010
dmdeh <131264106+dmdeh@users.noreply.github.com>
2024-12-30 13:55:16 +0900

    style: 컬러팔레트 추가

---
+++ src/styles/theme.ts
@@ -0,0 +1,15 @@
+export const theme = {
+  colors: {
+    pink100: "#fff0f6",
+    pink200: "#f9aecc",
+    purple100: "#ad9ef1",
+    purple200: "#9986ee",
+    gray100: "#f7f7f7",
+    gray200: "#c7d1db",
+    gray300: "#b6c3cb",
+    white: "#FFFFFF",
+    black: "#000000",
+  },
+};
+
+export default theme;
